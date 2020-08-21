b9d16ca1a533467655af965729199f8d489a9141
Kyle Bennion <bilekennion@hotmail.com>
2020-08-21 15:10:44 -0600

    added Upgrades array,changed vocab, click count

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
+// Global Variables
 let mainResource = 0
-let resource1 = 0;
-let resource2 = 0;
-let resource3 = 0;
-let resource4 = 0;
+let upGrade1 = 0;
+let upGrade2 = 0;
+let upGrade3 = 0;
+let upGrade4 = 0;
 
+
+// Upgrades
 let clickUpgrade = {
-
+  upGrade1: {
+    price: 100,
+    quantity: 0,
+    multiplier: 1
+  }
 }
 
 let autoUpgrade = {
-
+  upGrade2: {
+    price: 300,
+    quantity: 0,
+    multiplier: 3
+  },
+  upGrade3: {
+    price: 600,
+    quantity: 0,
+    multiplier: 6
+  },
+  upGrade4: {
+    price: 1000,
+    quantity: 0,
+    multiplier: 10
+  }
 }
 
+// Farming Functions
 
 function farm() {
   mainResource++
