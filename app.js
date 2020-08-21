b6cbe9b1e0da16e5d78dc69a76da24a4cad025d2
Kyle Bennion <bilekennion@hotmail.com>
2020-08-21 14:58:39 -0600

    Resource Card complete with incrementor

--- app.js
+++ app.js
@@ -1,43 +1,36 @@
-let resources = 0
+let mainResource = 0
+let resource1 = 0;
+let resource2 = 0;
+let resource3 = 0;
+let resource4 = 0;
+
+let clickUpgrade = {
+
+}
+
+let autoUpgrade = {
+
+}
 
 
-function gainResources() {
-  console.log('gained')
+function farm() {
+  mainResource++
+  updateScreen()
 }
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function drawResources() {
+  document.getElementById("mainResource").innerHTML = mainResource.toString()
+}
 
 
 
 
 function updateScreen() {
-
+  drawResources()
 }
 
 updateScreen()
